f011f8322dc9bd46e21201fe9c98cefed8da31b0
Lusinne <hovh.lus@bk.ru>
2019-01-18 18:26:30 +0400

    added numButtons

--- js/questions.js
+++ js/questions.js
@@ -1,34 +1,86 @@
 (function(){
-    window.questions = function(){
-        let section = $('.game');
-        let bigDiv = $('<div id="miniSection"></div>');
-        let points = $('<div id="points">Ձեր հաշիվը՝ 0</div>');
-        let title = $('<h2>Level 3: Number Game</h2>');
-        let big = $('<div id="big"><div id="questions"></div><input id="input" type="text" autofocus="autofocus" placeholder="Նշեք ճիշտ պատասխանը"></div>')
-        section.append(bigDiv.append(title,big,points));
-        $('#input')[0].focus();
-        let count = 0;
-        let randomMultiple = 100;
-        let t = 1500;
-        let set = [];
-        let int = setInterval(create, t, randomMultiple);
-        $('input').on('keyup',function(){
-            let alast = $('.a:last');
-            if($(this).val() === alast.text()){
-                alast.remove();
-                count++;
-                points.html('Ձեր հաշիվը՝ ' + count);
-                if(count % 10 === 0){
-                    count += 5;
-                    clearInterval(int);
-                    randomMultiple *= 10;
-                    int = setInterval(create,t-100,randomMultiple)
-                }
-                $(this).val('');
-            }
+    window.Questions = function(){
+        this.section = $('.game');
+        this.bigDiv = $('<div id="miniSection"></div>');
+        this.points = $('<h2 id="points">Ձեր հաշիվը՝ 0</h2>');
+        this.title = $('<h2>Level 3: Number Game</h2>');
+        this.big = $('<div id="big"></div>');
+        this.questions = $('<div id="questions"></div>');
+        this.input = $('<input id="input" type="text" placeholder="Նշեք ճիշտ պատասխանը">');
+        this.section.append(this.bigDiv.append(this.title,this.big.append(this.questions, this.input),this.points));
+        let self = this;
+        this.numList = addNumButtons(self.bigDiv,function(){
+            self.input[0].value += this.innerText;
+            checkInput();
+        }, function(){
+            self.input[0].value = self.input.val().slice(0,-1);
         });
+
+        this.play = function(){
+            this.count = 0;
+            this.randomMultiple = 100;
+            this.t = 1500;
+            this.set = [];
+            this.points.html('Ձեր հաշիվը՝ 0');
+            this.int = setInterval(create, self.t, self.randomMultiple);
+            this.input.prop('disabled', false);
+            this.input.val('');
+
+            if(window.orientation === undefined) self.input[0].focus();
+            else this.numList.css('display', 'flex');
+
+            bindEvents();
+        };
+
+        this.endGame = function(){
+            removeEvents();
+            // removeNumButtons();
+            clearInterval(self.int);
+            let a = $('.a');
+            a.stop(true,false);
+            a.remove();
+            self.input.prop('disabled', true);
+            self.set.forEach(function(v){
+                clearInterval(v);
+            });
+            self.set = [];
+            this.numList.css('display', 'none')
+        };
+
+        this.play();
+
+        function bindEvents(){
+            self.input.on('keyup', checkInput);
+            self.input.on('focus', function(){
+                self.numList.css('display', 'none');
+            });
+            self.input.on('blur', function(){
+                self.numList.css('display', 'flex');
+            })
+        }
+        function removeEvents(){
+            self.input.off('keyup', checkInput);
+        }
+
+        function checkInput(){
+            let alast = $('.a:last');
+            if(self.input.val() && self.input.val() === alast.text()){
+                alast.remove();
+                self.count++;
+                self.points.html('Ձեր հաշիվը՝ ' + self.count);
+                if(self.count % 10 === 0){
+                    clearInterval(self.int);
+                    self.randomMultiple *= 10;
+                    self.t -= 50;
+                    self.count++;
+                    self.int = setInterval(create, self.t, self.randomMultiple)
+                }
+                self.input.val('');
+            }
+        }
+
         function create(randomMultiple){
-            let div = $('<div class="a" style="position:relative;">'+Math.floor(Math.random()*randomMultiple)+'</div>');
+            let div = $('<div class="a" style="position:relative;">' + Math.floor(Math.random() * randomMultiple) + '</div>');
             div.animate({
                 paddingTop: '+=15px',
                 paddingBottom: '+=15px',
@@ -39,43 +91,31 @@
             },1500,function () {
                 div.animate({
                     top: '+=90vh'
-                },25000,'linear');
+                }, 25000, 'linear');
             });
 
+            self.questions.prepend(div);
 
-            $('#questions').prepend(div);
-
-            set.push(setInterval(function(){
+            self.set.push(setInterval(function(){
                 let divs = $('.a:last');
-                let input = $('input');
-                if(divs.position() && (divs.position().top + parseInt(divs.css('height'))*1.3)  >= input.position().top){
-                    clearInterval(int);
-                    $('.a').stop(true,false);
-                    input.prop('disabled', true);
-                    set.forEach(function(v){
-                        clearInterval(v);
-                    });
-                    if(count >= 40){
-                        openNextStage(2,'ballons');
-                        showAlert('Շնորհավորում ենք, Դուք հաղթահարեցիք երկրորդ փուլը: <br> Դուք հավաքել եք ' + count + ' միավոր', 2);
-                    }else{
-                        showAlert('Ձեզ պակասում է '+ (40 - count) +' միավոր հաջորդ փուլ անցնելու համար: Փորձեք նորից:');
-                        $('#questions').html('');
-                        playAgainButton(function(){
-                            input.prop('disabled', false);
-                            count = 0;
-                            randomMultiple = 100;
-                            t = 1500;
-                            set = [];
-                            points.html('Ձեր հաշիվը՝ ' + count);
-                            int = setInterval(create, t, randomMultiple);
-                            input[0].focus();
+                if(divs.position() && (divs.position().top + parseInt(divs.css('height'))*1.3)  >= self.input.position().top){
+                    self.endGame();
 
+                    if(self.count >= 40){
+                        openNextStage(2,'ballons');
+                        showAlert('Շնորհավորում ենք, Դուք հաղթահարեցիք երկրորդ փուլը: <br> Դուք հավաքել եք ' + self.count + ' միավոր', 2);
+                    }else{
+                        showAlert('Ձեզ պակասում է '+ (40 - self.count) +' միավոր հաջորդ փուլ անցնելու համար: Փորձեք նորից:');
+                        self.questions.html('');
+
+                        playAgainButton(function(){
+                            self.play();
                         })
                     }
                 }
             },150))
 
         }
-    }
+    };
+
 })();
